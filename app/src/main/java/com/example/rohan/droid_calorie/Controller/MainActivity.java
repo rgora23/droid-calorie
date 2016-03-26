527e26b40788bce13a8f4be7607fa855395987f3
Rohan Gorawala <r.gorawala2394@gmail.com>
2016-03-26 16:29:18 -0400

    Mostly trying to add the toolbar among few other things

--- app/src/main/java/com/example/rohan/droid_calorie/Controller/MainActivity.java
+++ app/src/main/java/com/example/rohan/droid_calorie/Controller/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rohan.droid_calorie.Controller;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.rohan.droid_calorie.R;
 
@@ -11,5 +13,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
