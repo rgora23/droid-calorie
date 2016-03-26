0f7aa1a53d2a4ec7254ed50b3e6d945fe6e86239
Josh <josh.b.sexton@gmail.com>
2016-03-26 14:47:29 -0400

    No change

--- app/src/main/java/com/example/rohan/droid_calorie/Controller/MacroActivity.java
+++ app/src/main/java/com/example/rohan/droid_calorie/Controller/MacroActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+
 import com.example.rohan.droid_calorie.R;
 
 public class MacroActivity extends AppCompatActivity {
