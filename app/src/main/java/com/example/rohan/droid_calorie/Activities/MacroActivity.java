b683e1655799356afd209cbd935a15c99c5e414a
Rohan Gorawala <r.gorawala2394@gmail.com>
2016-03-26 17:15:32 -0400

    Finished toolbar navigation within app

--- app/src/main/java/com/example/rohan/droid_calorie/Activities/MacroActivity.java
+++ app/src/main/java/com/example/rohan/droid_calorie/Activities/MacroActivity.java
@@ -1,8 +1,12 @@
 package com.example.rohan.droid_calorie.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 
 import com.example.rohan.droid_calorie.R;
@@ -14,9 +18,45 @@ public class MacroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_macro);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.food_activity_menu_button:
+                startActivityForResult(new Intent(MacroActivity.this, FoodActivity.class), 0);
+                return true;
+
+            case R.id.user_activity_menu_button:
+                startActivityForResult(new Intent(MacroActivity.this, AccountActivity.class), 0);
+                return true;
+
+            case R.id.macro_activity_menu_button:
+                startActivityForResult(new Intent(MacroActivity.this, MacroActivity.class), 0);
+                return true;
+
+            case R.id.dash_activity_menu_button:
+                startActivityForResult(new Intent(MacroActivity.this, DashActivity.class), 0);
+                return true;
+
+            case R.id.day_activity_menu_button:
+                startActivityForResult(new Intent(MacroActivity.this, DayDetail.class), 0);
+                return true;
+
+        }
 
 
+        return true;
     }
 
 }
--- app/src/main/java/com/example/rohan/droid_calorie/Activities/MainActivity.java
+++ app/src/main/java/com/example/rohan/droid_calorie/Activities/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.rohan.droid_calorie.Activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.rohan.droid_calorie.R;
 
@@ -19,6 +21,37 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.food_activity_menu_button:
+                startActivityForResult(new Intent(MainActivity.this, FoodActivity.class), 0);
+                return true;
+
+            case R.id.user_activity_menu_button:
+                startActivityForResult(new Intent(MainActivity.this, AccountActivity.class), 0);
+                return true;
+
+            case R.id.macro_activity_menu_button:
+                startActivityForResult(new Intent(MainActivity.this, MacroActivity.class), 0);
+                return true;
+
+            case R.id.dash_activity_menu_button:
+                startActivityForResult(new Intent(MainActivity.this, DashActivity.class), 0);
+                return true;
+
+            case R.id.day_activity_menu_button:
+                startActivityForResult(new Intent(MainActivity.this, DayDetail.class), 0);
+                return true;
+
+        }
+
+
         return true;
     }
 }
